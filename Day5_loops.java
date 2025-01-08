94b85c3d9a9c2fdf85824459d986bc252ebfbb5f
bhumika309 <surapanenibhumika30@gmail.com>
2025-01-08 18:26:37 +0530

    Create Day5_loops.java

---
+++ Day5_loops.java
@@ -0,0 +1,11 @@
+  /*Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+stringTimes("Hi", 2) → "HiHi"
+stringTimes("Hi", 3) → "HiHiHi"
+stringTimes("Hi", 1) → "Hi" */
+public String stringTimes(String str, int n) {
+  String result = "";
+  for (int i=0; i<n; i++) {
+    result = result + str;  // could use += here
+  }
+  return result;
+}
